a55c67fd10ac6dab63c57761ba4d36dbb7f36f83
gushul <ueieko@gmail.com>
2014-08-22 17:35:12 +0400

    add random to new_review metod

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,8 @@ class Card < ActiveRecord::Base
   validates :original_text, :translated_text, presence: true
 
   def self.new_review
-    self.select { |card| card.review_date.to_i <= Time.now.to_i }
+    find_review = self.select { |card| card.review_date.to_i <= Time.now.to_i }
+    return find_review.sample(1)
   end
+
 end
