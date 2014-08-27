f929d214b89a02bf93cc1c484ef61c703b0430dd
gushul <ueieko@gmail.com>
2014-08-27 17:26:56 +0400

    Dry and clean

--- spec/model/card_spec.rb
+++ spec/model/card_spec.rb
@@ -1,22 +1,29 @@
 require 'rails_helper'
 
-
-RSpec.describe Card, :type => :model do
+describe Card do
   before do
     @card = Card.create!(original_text: "Hello", translated_text: "Hi", review_date: Time.now )
   end
-  it "method chek_translation must return true" do
-    expect(@card.check_translation("Hi")).to eq true
+  
+  context "#check_translation" do
+    it "with valid data" do
+      expect(@card.check_translation("Hi")).to eq true
+    end
+  
+    it "with invalid data" do
+      expect(@card.check_translation("Hii")).to eq false
+    end
   end
-  it "method chek_translation must return false" do
-    expect(@card.check_translation("Hii")).to eq false
-  end
-  it "method update_review_date must change Date + 3 day" do
-    @card.update_review_date
-    expect(@card.review_date.to_date).to eq(Date.today + 3.day) 
-  end
-  it "method update_review_date must be false" do
-    @card.update_review_date
-    expect(@card.review_date.to_date).to_not eq(Date.today) 
+  
+  context "#update_review_date" do
+    it "changes review date by 3 day" do
+      @card.update_review_date
+      expect(@card.review_date.to_date).to eq(Date.today + 3.day) 
+    end
+  
+    it "changes review date & and raview date not today" do
+        @card.update_review_date
+      expect(@card.review_date.to_date).to_not eq(Date.today) 
+    end
   end
 end
