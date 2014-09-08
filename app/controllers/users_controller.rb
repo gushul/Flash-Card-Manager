099d73840f4e447c4bb25c437b3fc63cac0ddc3d
gushul <ueieko@gmail.com>
2014-09-08 09:15:47 +0400

    Added sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url, notice: 'User was successfully created.'
+      sing_in @user
+      redirect_to root_url, notice: 'Welcome'
     else
       render :new
     end
